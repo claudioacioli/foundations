7355d8331c2978b43c47f008c32fbc6920a7b709
Claudio Acioli <kllaudyo@gmail.com>
2019-07-13 20:45:37 -0300

    chore: Cria arquivo bat inicializador

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -8,9 +8,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
 
 
---
+++ start.bat
@@ -0,0 +1,6 @@
+REM init.bat 
+
+set FLASK_DEBUG=1
+set FLASK_RUN=app.py
+
+flask run --host 0.0.0.0 --port 5000
