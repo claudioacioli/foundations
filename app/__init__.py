750b0fdedc1dec0598d596bf6aa0de2f9dfea595
Claudio Acioli <kllaudyo@gmail.com>
2019-07-14 18:14:36 -0300

    refactor: Remove codigo nao utilizado

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config
@@ -8,7 +8,6 @@ db = SQLAlchemy()
 
 
 def create_app(config_name):
-
     app = Flask(__name__)
     app.config.from_object(config[config_name])
 
