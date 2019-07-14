41c55d31f2737dd32cfd38c40be67ba255fb358b
Claudio Acioli <kllaudyo@gmail.com>
2019-07-13 21:07:09 -0300

    feat: Adiciona conexao com banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'restaurantmenu.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
