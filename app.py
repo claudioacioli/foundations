594de87b3b438568c181db46fdbf085cb1c5c073
Claudio Acioli <kllaudyo@gmail.com>
2019-07-13 21:12:36 -0300

    feat: Cria classes de acesso a banco de dados

--- app.py
+++ app.py
@@ -10,6 +10,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
+
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_item'
+
+    name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    course = db.Column(db.String(250))
+    description = db.Column(db.String(250))
+    price = db.Column(db.String(8))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    restaurant = db.relationship(Restaurant)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
