ace746e699d9ed78fe2b1c13b314e04760a3e9ba
Claudio Acioli <kllaudyo@gmail.com>
2019-07-14 00:02:46 -0300

    feat: Cria rotas

--- app.py
+++ app.py
@@ -32,9 +32,49 @@ class MenuItem(db.Model):
     restaurant = db.relationship(Restaurant)
 
 
+@app.route('/restaurants/')
 @app.route('/')
-def index():
+def show_restaurants():
     return render_template('index.html')
 
+
+@app.route('/restaurants/new/')
+def create_restaurant():
+    return 'create a new restaurant here'
+
+
+@app.route('/restaurants/<int:restaurant_id>/edit/')
+def edite_restaurant(restaurant_id):
+    return 'edit you restaurant here'
+
+
+@app.route('/restaurants/<int:restaurant_id>/delete/')
+def delete_restaurant(restaurant_id):
+    return 'delte you restaurant here'
+
+
+@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurants/<int:restaurant_id>/menu/')
+def show_menu(restaurant_id):
+    return 'this page is the menu for restaurant'
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/')
+def show_menu_item(restaurant_id, menu_id):
+    return ''
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/new/')
+def create_menu_item(restaurant_id):
+    return 'this page is for making a new menu item for restaurant'
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def edit_menu_item(restaurant_id, menu_id):
+    return ''
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def delete_menu_item(restaurant_id, menu_id):
+    return ''
 
 
