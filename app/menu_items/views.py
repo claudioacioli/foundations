78bf9175972bbc41e70b64e70746c7a1cf62c73b
Claudio Acioli <kllaudyo@gmail.com>
2019-07-14 22:18:55 -0300

    feat: Manipula post formulario criacao

--- app/menu_items/views.py
+++ app/menu_items/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from .. import db
 from .forms import CreateMenuItem
 from ..models import MenuItem
@@ -11,8 +11,25 @@ def show_menu(restaurant_id):
     return render_template('menus.html', menu_items=menu_items, restaurant_id=restaurant_id)
 
 
-@app_menu_items.route('/new', methods=['GET'])
+@app_menu_items.route('/new', methods=['GET', 'POST'])
 def create_menu(restaurant_id):
     if request.method == 'GET':
         return render_template('create_menu_item.html', form=CreateMenuItem(), restaurant_id=restaurant_id)
+
+    if request.method == 'POST':
+        form = CreateMenuItem()
+        if form.validate_on_submit():
+            menu_item = MenuItem()
+            menu_item.name = form.name.data
+            menu_item.description = form.description.data
+            menu_item.course = form.course.data
+            menu_item.price = form.price.data
+            menu_item.restaurant_id = restaurant_id
+            db.session.add(menu_item)
+            db.session.commit()
+            flash('New Menu item created!')
+        else:
+            flash('Unspected error on create menu item!')
+
+        return redirect(url_for('.show_menu', restaurant_id=restaurant_id))
 
