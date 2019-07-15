c8e51d8371e8332afa1be9823890a49b253e550e
Claudio Acioli <kllaudyo@gmail.com>
2019-07-14 21:50:57 -0300

    fix: Passa id para template

--- app/menu_items/views.py
+++ app/menu_items/views.py
@@ -7,4 +7,4 @@ from . import menu_items as app_menu_items
 @app_menu_items.route('/')
 def show_menu(restaurant_id):
     menu_items = db.session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
-    return render_template('menus.html', menu_items=menu_items)
+    return render_template('menus.html', menu_items=menu_items, restaurant_id=restaurant_id)
